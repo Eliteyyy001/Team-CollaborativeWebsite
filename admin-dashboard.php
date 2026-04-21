59906846d6faf0f760662a9ddaf170461a2d049f
Hannah Injety <hinjety@gmail.com>
2026-04-21 15:04:17 -0400

    Update admin-dashboard.php
    updating nav bar

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -149,6 +149,7 @@ if (isset($_GET['edit'])) {
         <div class="nav-brand">Freshfold Admin</div>
         <ul class="nav-links">
         <li><a href="admin-dashboard.php"class="active">Users</a></li>
+		<li><a href="products.php">Products</a></li>
         <li><a href="admin-alerts.php">Alerts</a></li>
         <li><a href="display_charts.php">Charts</a></li>
         <li><a href="top_selling_report.php" >Reports</a></li>
